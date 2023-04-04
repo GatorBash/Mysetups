aa6552952b9b78c93724e64d3b2ccabf9dfa1b09
Holden Caulfield <95302944+GatorBash@users.noreply.github.com>
2023-04-04 15:54:02 +0300

    Update kali.sh

--- distros/kali.sh
+++ distros/kali.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ $UID != 0 ]]
+then
+  echo 'run it as root'
+  exit 1
+fi
+
 cat > /tmp/dependencies << EOF
 gpsd
 gpsd-tools
@@ -7,9 +13,12 @@ wireguard
 wireguard-tools
 dkms
 wireguard-dkms
+EOF
 
 apt -q update
 for pkg in $(cat /tmp/dependencies
 do
   sudo apt install -y $pkg
 done
+echo 'done for now'
+sleep 4
